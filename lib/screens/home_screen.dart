10ed02e602c714c2b6d4ba9579bc5a3815adeb9a
Ankit Kumar <karan180898@gmail.com>
2023-07-31 14:54:21 +0530

    Created todos and reflecting in screen done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:your_note/services/dbhelper/todo_helper.dart';
+
+import '../models/db_models/todo_model.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final dbHelper = TodoDBHelper();
+  final TextEditingController textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -9,23 +16,50 @@ class HomeScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet<void>(
-            isDismissible: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(topRight: Radius.circular(20),topLeft: Radius.circular(20))
-            ),
+              isDismissible: true,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20),
+                      topLeft: Radius.circular(20))),
               context: context,
               builder: (BuildContext context) {
                 return SizedBox(
                   height: 200,
                   child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        const Text('Modal BottomSheet'),
-                        ElevatedButton(
-                          child: const Text('Close BottomSheet'),
-                          onPressed: () => Navigator.pop(context),
+                    child: ListView(
+                      children: [
+                        FilledButton(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor:
+                                Theme.of(context).colorScheme.onPrimary,
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primary,
+                          ).copyWith(
+                              elevation: ButtonStyleButton.allOrNull(0.0)),
+                          onPressed: () {},
+                          child: const Text('Filled'),
+                        ),
+                        FilledButton(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor:
+                                Theme.of(context).colorScheme.onPrimary,
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primary,
+                          ).copyWith(
+                              elevation: ButtonStyleButton.allOrNull(0.0)),
+                          onPressed: () {},
+                          child: const Text('Filled'),
+                        ),
+                        FilledButton(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor:
+                                Theme.of(context).colorScheme.onPrimary,
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primary,
+                          ).copyWith(
+                              elevation: ButtonStyleButton.allOrNull(0.0)),
+                          onPressed: () {},
+                          child: const Text('Filled'),
                         ),
                       ],
                     ),
@@ -35,10 +69,11 @@ class HomeScreen extends StatelessWidget {
         },
         child: Icon(Icons.add),
       ),
-      drawer:Drawer(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(topRight: Radius.circular(20),bottomRight: Radius.circular(20))
-        ),
+      drawer: Drawer(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(20),
+                  bottomRight: Radius.circular(20))),
           child: Column(
             children: [
               Expanded(
@@ -46,12 +81,13 @@ class HomeScreen extends StatelessWidget {
                   // Important: Remove any padding from the ListView.
                   padding: EdgeInsets.only(top: 30),
                   children: [
-
                     Padding(
                       padding: const EdgeInsets.only(left: 10.0),
                       child: Text("Todoify"),
                     ),
-                    const SizedBox(height: 20,),
+                    const SizedBox(
+                      height: 20,
+                    ),
                     ListTile(
                       leading: Icon(
                         Icons.home,
@@ -70,8 +106,8 @@ class HomeScreen extends StatelessWidget {
                         Navigator.pop(context);
                       },
                     ),
-
-                  ],),
+                  ],
+                ),
               ),
               // Spacer(), // <-- This will fill up any free-space
               // Everything from here down is bottom aligned in the drawer
@@ -81,57 +117,134 @@ class HomeScreen extends StatelessWidget {
                 leading: Icon(Icons.settings),
               ),
             ],
-          ))
-      ,
+          )),
       appBar: AppBar(
         elevation: 0,
         actions: [
-          IconButton(onPressed: (){}, icon:Icon(Icons.settings_outlined))
+          IconButton(onPressed: () {}, icon: Icon(Icons.settings_outlined))
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 15.0,right: 15.0,top: 15.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Todoify",),
-            const SizedBox(height: 20,),
-            Text("What's up "),
-            const SizedBox(height: 15,),
-            Text("Your notes:"),
-            const SizedBox(height: 10,),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.20,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(20)
-                
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Todoify",
               ),
-            ),
-            const SizedBox(height: 10,),
-            Text("Your todos:"),
-            const SizedBox(height: 10,),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.20,
-              decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(20)
-
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            const SizedBox(height: 10,),
-            Text("Your scheduled tasks:"),
-            const SizedBox(height: 10,),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.20,
-              decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(20)
-
+              Text("What's up "),
+              const SizedBox(
+                height: 15,
               ),
-            )
-
-          ],
+              Text("Your notes:"),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.20,
+                decoration: BoxDecoration(
+                    color: Colors.grey, borderRadius: BorderRadius.circular(20)),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text("Your todos:"),
+              const SizedBox(
+                height: 10,
+              ),
+              Obx(() {
+                final List<Todo> todos = dbHelper.todos;
+                if (todos.isEmpty) {
+                  return InkWell(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Add'),
+                            content: TextField(
+                              controller: textController,
+                              decoration: InputDecoration(
+                                hintText: 'Todo',
+                              ),
+                            ),
+                            actions: <Widget>[
+                              TextButton(
+                                child: const Text('Add'),
+                                onPressed: () async {
+                                  if (textController.text.isNotEmpty) {
+                                    Todo newTodo = Todo(
+                                      id: DateTime.now().millisecondsSinceEpoch,
+                                      title: textController.text,
+                                    );
+                                    await dbHelper.insertTodo(newTodo);
+                                    textController.clear();
+                                    dbHelper.getTodos();
+                                    Get.back();
+                                  }
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.20,
+                      decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Center(child: Text("Empty! Please create a todo")),
+                    ),
+                  );
+                } else {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: todos.length,
+                    itemBuilder: (context, index) {
+                      print("lenght: ${todos.length}");
+                      final Todo todo = todos[index];
+                      return ListTile(
+                        title: Text(todo.title),
+                        leading: Checkbox(
+                          value: todo.isCompleted,
+                          onChanged: (value) async {
+                            todo.isCompleted = value ?? false;
+                            await dbHelper.updateTodoStatus(todo);
+                            dbHelper.getTodos();
+                          },
+                        ),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () async {
+                            await dbHelper.deleteTodo(todo.id);
+                            dbHelper.getTodos();
+                          },
+                        ),
+                      );
+                    },
+                  );
+                }
+              }),
+              const SizedBox(
+                height: 10,
+              ),
+              Text("Your scheduled tasks:"),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.20,
+                decoration: BoxDecoration(
+                    color: Colors.grey, borderRadius: BorderRadius.circular(20)),
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/services/dbhelper/todo_helper.dart
@@ -0,0 +1,77 @@
+import 'dart:async';
+import 'package:get/get.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import '../../models/db_models/todo_model.dart';
+
+class TodoDBHelper {
+  static final TodoDBHelper _instance = TodoDBHelper._internal();
+
+  factory TodoDBHelper() => _instance;
+
+  TodoDBHelper._internal();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await initDB();
+    return _database!;
+  }
+
+  Future<Database> initDB() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, 'todo.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE todos(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            isCompleted INTEGER
+          )
+        ''');
+      },
+    );
+  }
+
+  Future<void> insertTodo(Todo todo) async {
+    final db = await database;
+    await db.insert(
+      'todos',
+      todo.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  RxList<Todo> todos = <Todo>[].obs;
+
+  Future<void> getTodos() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('todos');
+    todos.value = List.generate(maps.length, (i) {
+      return Todo.fromMap(maps[i]);
+    });
+  }
+
+  Future<void> updateTodoStatus(Todo todo) async {
+    final db = await database;
+    await db.update(
+      'todos',
+      todo.toMap(),
+      where: 'id = ?',
+      whereArgs: [todo.id],
+    );
+  }
+
+  Future<void> deleteTodo(int id) async {
+    final db = await database;
+    await db.delete(
+      'todos',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
